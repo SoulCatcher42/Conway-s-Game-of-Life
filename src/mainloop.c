893b01e1ada544a6ead0febb33f44cd5b341b04d
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2018-06-05 01:46:30 +0700

    Remove functions from mainloop

--- src/mainloop.c
+++ src/mainloop.c
@@ -1,13 +1,6 @@
 #include "mainloop.h"
-#include "field.h"
-#include "field_print.h"
 
 void mainloop()
 {
-    char field[FIELD_HEIGHT][FIELD_WIDTH];
-    field_init(field);
-    random_fill(field);
-    int gen = 0;
-    int live = 0;
-    field_print(field, live, gen);
+
 }
