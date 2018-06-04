8fcd030dba8f19323af27965da629c4fd6413785
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2018-06-05 00:09:11 +0700

    Add menu_print func to menu.[ch]

---
+++ src/menu.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "menu.h"
+
+void menu_print()
+{
+    printf("1. Random generation\n");
+    printf("2. Pre-set of figures\n");
+    printf("3. Reading a figure from a file\n");
+}
