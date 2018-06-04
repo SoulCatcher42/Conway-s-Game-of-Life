ac5186d19864d6df3f87e95070cd4818bb9f8886
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2018-06-05 00:11:24 +0700

    Add clear screen to menu.c

--- src/menu.c
+++ src/menu.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "menu.h"
 
 void menu_print()
 {
+    system("clear");
     printf("1. Random generation\n");
     printf("2. Pre-set of figures\n");
     printf("3. Reading a figure from a file\n");
