cf66d5369440939fdcc8e68cf1d388fed9777402
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2018-06-06 15:00:59 +0700

    Add switches to menu.c and fix getting input

--- src/menu.c
+++ src/menu.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "menu.h"
+#include "field.h"
+#include "mainloop.h"
 
 void menu_print()
 {
     system("clear");
     printf("1. Random generation\n");
-    printf("2. Pre-set of figures\n");
+    printf("2. Preset of figures\n");
     printf("3. Reading a figure from a file\n");
 }
 
@@ -17,14 +19,32 @@ int check_input(char input[2])
 
 void menu()
 {
+    char field[FIELD_HEIGHT][FIELD_WIDTH];
+    field_init(field);
     menu_print();
-    char chose[2];
+    char chose;
     while (1) {
-        scanf("%s", chose);
-        if (!check_input(chose)) {
+        char temp[2];
+        scanf("%s", temp);
+        if (!check_input(temp)) {
+            chose = temp[0];
             break;
         } else {
-            printf("Wrong input, please enter 1-3");
+            printf("Wrong input, please input 1-3\n");
         }
+    }
+    switch (chose) {
+    case '1':
+        random_fill(field);
+        mainloop();
+        break;
+    case '2':
+        // preset(field);
+        mainloop();
+        break;
+    case '3':
+        // read_file(field);
+        mainloop();
+        break;
     }
 }
