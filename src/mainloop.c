b7451040f451fbdd9849887ccaea1ab5c8d6140f
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2018-06-22 00:49:39 +0700

    Add live_cells func to mainloop.[ch]

--- src/mainloop.c
+++ src/mainloop.c
@@ -1,6 +1,25 @@
+#include <stdlib.h>
 #include "mainloop.h"
+#include "field_print.h"
+
+int live_cells(char field[FIELD_HEIGHT][FIELD_WIDTH])
+{
+    int live = 0;
+    for (int i = 0; i < FIELD_HEIGHT; i++) {
+        for (int j = 0; j < FIELD_WIDTH; j++) {
+            if (field[i][j] == '*') {
+                ++live;
+            }
+        }
+    }
+    return live;
+}
 
 void mainloop(char field[FIELD_HEIGHT][FIELD_WIDTH])
 {
-
+    int gen = 1;
+    while (1) {
+        system("clear");
+        field_print(field, gen, live)
+    }
 }
--- src/mainloop.h
+++ src/mainloop.h
@@ -2,4 +2,5 @@
 
 #include "field.h"
 
+int live_cells(char field[FIELD_HEIGHT][FIELD_WIDTH]);
 void mainloop(char field[FIELD_HEIGHT][FIELD_WIDTH]);
