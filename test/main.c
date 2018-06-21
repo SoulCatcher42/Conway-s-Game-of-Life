3280c0fa97da3a729c28fbdbd645f135bedeb53c
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2018-06-21 21:34:10 +0700

    Add tests for correction_coordinates func

--- test/main.c
+++ test/main.c
@@ -4,6 +4,7 @@
 
 #include <field.h>
 #include <menu.h>
+#include <cell.h>
 
 CTEST(field_init, correct)
 {
@@ -131,6 +132,104 @@ CTEST(check_input, incorrect9)
     char input[2] = "\\";
     int result = check_input(input);
     int expected = 1;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(correcting_coordinates, no_correction1)
+{
+    int result = correcting_coordinate(5, 10);
+    int expected = 5;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(correcting_coordinates, no_correction2)
+{
+    int result = correcting_coordinate(0, 10);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(correcting_coordinates, no_correction3)
+{
+    int result = correcting_coordinate(9, 10);
+    int expected = 9;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(correcting_coordinates, no_correction4)
+{
+    int result = correcting_coordinate(999, 1000);
+    int expected = 9;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(correcting_coordinates, over_limit1)
+{
+    int result = correcting_coordinate(10, 10);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(correcting_coordinates, over_limit2)
+{
+    int result = correcting_coordinate(1000, 1000);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(correcting_coordinates, over_limit3)
+{
+    int result = correcting_coordinate(11, 10);
+    int expected = 1;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(correcting_coordinates, over_limit4)
+{
+    int result = correcting_coordinate(55, 50);
+    int expected = 5;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(correcting_coordinates, over_limit5)
+{
+    int result = correcting_coordinate(30, 10);
+    int expected = 20;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(correcting_coordinates, less_than_zero1)
+{
+    int result = correcting_coordinate(-1, 10);
+    int expected = 9;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(correcting_coordinates, less_than_zero2)
+{
+    int result = correcting_coordinate(-5, 10);
+    int expected = 5;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(correcting_coordinates, less_than_zero3)
+{
+    int result = correcting_coordinate(-1, 1000);
+    int expected = 999;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(correcting_coordinates, less_than_zero4)
+{
+    int result = correcting_coordinate(-10, 10);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(correcting_coordinates, less_than_zero5)
+{
+    int result = correcting_coordinate(-400, 500);
+    int expected = 100;
     ASSERT_EQUAL(expected, result);
 }
 
