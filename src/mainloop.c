c1d82bb758988dec279e551b64ebc0b67460ad86
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2018-06-22 02:00:04 +0700

    Add iteration func and fix mainloop func

--- src/mainloop.c
+++ src/mainloop.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include "mainloop.h"
 #include "field_print.h"
+#include "cell.h"
 
 int live_cells(char field[FIELD_HEIGHT][FIELD_WIDTH])
 {
@@ -15,11 +18,28 @@ int live_cells(char field[FIELD_HEIGHT][FIELD_WIDTH])
     return live;
 }
 
+void iteration(char field[FIELD_HEIGHT][FIELD_WIDTH])
+{
+    char current_field[FIELD_HEIGHT][FIELD_WIDTH];
+    memcpy(current_field, field, sizeof(current_field));
+    field_init(field);
+    for (int i = 0; i < FIELD_HEIGHT; i++) {
+        for (int j = 0; j < FIELD_WIDTH; j++) {
+            if (cell_status(current_field, i, j)) {
+                field[i][j] = '*';
+            }
+        }
+    }
+}
+
 void mainloop(char field[FIELD_HEIGHT][FIELD_WIDTH])
 {
-    int gen = 1;
+    int gen = 0;
     while (1) {
+        int live = live_cells(field);
         system("clear");
-        field_print(field, gen, live)
+        field_print(field, ++gen, live);
+        iteration(field);
+        sleep(1);
     }
 }
