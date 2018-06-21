d4d5937d61b8959f58425e95d6e8be123de49e7f
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2018-06-22 02:33:20 +0700

    Add one element to menu

--- src/menu.c
+++ src/menu.c
@@ -10,41 +10,46 @@ void menu_print()
     printf("1. Random generation\n");
     printf("2. Preset of figures\n");
     printf("3. Reading a figure from a file\n");
+    printf("4. Exit\n");
 }
 
 int check_input(char input[2])
 {
-    return (input[1] == '\0' && (input[0] >= '1' && input[0] <= '3')) ? 0 : 1;
+    return (input[1] == '\0' && (input[0] >= '1' && input[0] <= '4')) ? 0 : 1;
 }
 
 void menu()
 {
     char field[FIELD_HEIGHT][FIELD_WIDTH];
-    field_init(field);
-    menu_print();
-    char chose;
     while (1) {
-        char temp[2];
-        scanf("%s", temp);
-        if (!check_input(temp)) {
-            chose = temp[0];
-            break;
-        } else {
-            printf("Wrong input, please input 1-3\n");
+        field_init(field);
+        menu_print();
+        char chose;
+        while (1) {
+            char temp[2];
+            scanf("%s", temp);
+            if (!check_input(temp)) {
+                chose = temp[0];
+                break;
+            } else {
+                printf("Wrong input, please input 1-3\n");
+            }
         }
-    }
-    switch (chose) {
-    case '1':
-        random_fill(field);
-        mainloop(field);
-        break;
-    case '2':
-        // preset(field);
-        mainloop(field);
-        break;
-    case '3':
-        // read_file(field);
-        mainloop(field);
-        break;
+        switch (chose) {
+        case '1':
+            random_fill(field);
+            mainloop(field);
+            break;
+        case '2':
+            // preset(field);
+            mainloop(field);
+            break;
+        case '3':
+            // read_file(field);
+            mainloop(field);
+            break;
+        case '4':
+            return;
+        }
     }
 }
