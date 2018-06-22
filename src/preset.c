14fe789d2e59e59f103acd169d8481030749f669
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2018-06-23 00:46:48 +0700

    Add preset_menu func to preset.[ch]

--- src/preset.c
+++ src/preset.c
@@ -16,3 +16,36 @@ int preset_menu_check_input(char input[2])
 {
     return !(input[1] == '\0' && (input[0] >= '1' && input[0] <= '5'));
 }
+
+void preset_menu(char field[FIELD_HEIGHT][FIELD_WIDTH])
+{
+    preset_menu_print();
+    char chose;
+    while (1) {
+        char temp[2];
+        scanf("%s", temp);
+        if (!preset_menu_check_input(temp)) {
+            chose = temp[0];
+            break;
+        } else {
+            printf("Wrong input, please input 1-5\n");
+        }
+    }
+    switch (chose) {
+    case '1':
+        gliger(field);
+        break;
+    case '2':
+        glider_gun(field);
+        break;
+    case '3':
+        pulsar(field);
+        break;
+    case '4':
+        spaceship(field);
+        break;
+    case '5':
+        pentadecathlon(field);
+        break;
+    }
+}
--- src/preset.h
+++ src/preset.h
@@ -1,4 +1,7 @@
 #pragma once
 
+#include "field.h"
+
 void preset_menu_print();
 int preset_menu_check_input(char input[2]);
+void preset_menu(char field[FIELD_HEIGHT][FIELD_WIDTH]);
