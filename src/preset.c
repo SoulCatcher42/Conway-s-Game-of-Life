907e95b8619921ea6cd64012ae08a4010038208d
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2018-06-23 01:12:39 +0700

    Add glider_gun to preset.[ch]

--- src/preset.c
+++ src/preset.c
@@ -58,3 +58,43 @@ void glider(char field[FIELD_HEIGHT][FIELD_WIDTH)
     field[11][50] = '*';
     field[10][49] = '*';
 }
+
+void glider_gun(char field[FIELD_HEIGHT][FIELD_WIDTH)
+{
+    field[6][2] = '*';
+    field[6][3] = '*';
+    field[7][2] = '*';
+    field[7][3] = '*';
+    field[6][12] = '*';
+    field[7][12] = '*';
+    field[8][12] = '*';
+    field[5][13] = '*';
+    field[9][13] = '*';
+    field[4][14] = '*';
+    field[10][14] = '*';
+    field[4][15] = '*';
+    field[10][15] = '*';
+    field[7][16] = '*';
+    field[5][17] = '*';
+    field[9][17] = '*';
+    field[6][18] = '*';
+    field[7][18] = '*';
+    field[8][18] = '*';
+    field[7][19] = '*';
+    field[4][22] = '*';
+    field[5][22] = '*';
+    field[6][22] = '*';
+    field[4][23] = '*';
+    field[5][23] = '*';
+    field[6][23] = '*';
+    field[3][24] = '*';
+    field[7][24] = '*';
+    field[2][26] = '*';
+    field[3][26] = '*';
+    field[7][26] = '*';
+    field[8][26] = '*';
+    field[4][36] = '*';
+    field[5][36] = '*';
+    field[4][37] = '*';
+    field[5][37] = '*';
+}
